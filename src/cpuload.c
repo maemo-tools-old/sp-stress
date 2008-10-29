c62beb401e1f2cdfb46f7321f1726833917d4871
Eero Tamminen <eero.tamminen@nokia.com>
2008-10-29 14:59:18 +0000

    - support for:   - selecting lowest/highest priority, or   - selecting process scheduler - better usage output

--- src/cpuload.c
+++ src/cpuload.c
@@ -43,13 +43,18 @@
 
 #include <sys/time.h>
 #include <sys/types.h>
-
+#include <linux/sched.h>
+#include <sched.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <errno.h>
+#include <ctype.h>
+
+#define FALSE 0
+#define TRUE 1
 
 /* ========================================================================= *
  * Definitions.
@@ -181,30 +186,135 @@ static void generate_load(unsigned load)
 } /* generate_load */
 
 /* ========================================================================= *
+ * Set nice value
+ * ========================================================================= */
+static int set_nice(int change)
+{
+   /* Nice() may return -1 even if it succeeded,
+    * thus need to check errno
+    */
+   errno = 0;
+   if (nice(19) < 0 && errno != 0)
+     perror ("\nWARNING: setting priority failed: operating with default.\nReason");
+   return TRUE;
+}
+
+/* ========================================================================= *
+ * Set process scheduler
+ * ========================================================================= */
+static int set_sched(int policy, int load)
+{
+   struct sched_param param;
+   
+   if ((policy == SCHED_RR || policy == SCHED_FIFO)
+       && (load == 0 || load > 90))
+   {
+      fprintf(stderr, "\nERROR: unsuitable %d load (random or >90) selected for real-time scheduling.\n", load);
+      return FALSE;
+   }
+   
+   memset(&param, 0, sizeof(param));
+   /* use the highest priority for given scheduler policy */
+   param.sched_priority = sched_get_priority_max(policy);
+   if (sched_setscheduler(0, policy, &param) < 0)
+     perror("\nWARNING: setting scheduler failed failed: operating with default.\nReason");
+
+   return TRUE;
+}
+
+/* ========================================================================= *
+ *Argument parsing, return FALSE for failure
+ * ========================================================================= */
+static int parse_args(int argc, const char* argv[], unsigned *load)
+{
+   char sched_pol;
+   
+   if (argc < 2 || argc > 4)
+     return FALSE;
+
+   *load = strtoul(argv[argc-1], NULL, 0);
+
+   if (argc == 2)
+     return TRUE;
+
+   /* backwards compatibility */
+   if (argc == 3)
+   {
+      /* nice to highest priority? */
+      if (!strncmp(argv[1], "-p", 2))
+	return set_nice(-19);
+      return FALSE;
+   }
+   
+   /* not "-s ."? */
+   sched_pol = argv[2][0];
+   if (argv[1][0] != '-' || argv[1][1] != 's' || argv[1][2]
+       || !sched_pol || argv[2][1])
+     return FALSE;
+
+
+   /* which scheduling policy requested? */
+   switch(tolower(sched_pol))
+   {
+      /* nice to lowest priority */
+   case 'l':
+      return set_nice(19);
+      /* nice to highest priority */
+   case 'h':
+      return set_nice(-19);
+      /* batch scheduler */
+   case 'b':
+      return set_sched(SCHED_BATCH, *load);
+      /* scheduler that goes over default SCHED_OTHER */
+   case 'f':
+      return set_sched(SCHED_FIFO, *load);
+      /* SCHED_FIFO with round robin policy */
+   case 'r':
+      return set_sched(SCHED_RR, *load);
+      /* default scheduler */
+   case 'o':
+      return set_sched(SCHED_OTHER, *load);
+   default:
+      fprintf(stderr, "\nERROR: Unknown scheduling policy / priority '%c'.\n", sched_pol);
+      return FALSE;
+   }
+}
+
+/* ========================================================================= *
  * Main function of CPU load generator.
  * ========================================================================= */
 
 int main(int argc, const char* argv[])
 {
-   const unsigned load = ( (3 == argc || 2 == argc) ?
-	 strtoul(argv[argc-1], NULL, 0) : 0);
+   const char *name;
+   unsigned int load = 0;
 
-   printf ("cpu load generator, build %s %s\n", __DATE__, __TIME__);
-   printf ("2006,2008 (c) Nokia\n");
-   if (3 == argc || 2 == argc)
+   printf ("\nCPU load generator, build %s %s.\n", __DATE__, __TIME__);
+   printf ("Copyright (C) 2006,2008 Nokia Corporation.\n");
+   
+   if (parse_args(argc, argv, &load))
    {
-    /* Nice() may return -1 even if it succeeded, thus need to check errno */
-    errno = 0;
-    if ( 3 == argc && !strncmp(argv[1], "-p", 2) && nice(-19) < 0 &&
-	 errno != 0) 
-      perror ("set highest priority failed: operating with default. Reason");
       calibrate_cpu();
       generate_load(load);
+      return 0;
    }
+   /* basename */
+   name = strrchr(argv[0], '/');
+   if (name)
+     name++;
    else
-   {
-      printf ("usage: %s [-p] HIGHEST_CPU_LOAD_PERCENTAGE [0 means random load]\n", argv[0]);
-   }
-
-   return 0;
+     name = argv[0];
+   /* usage */
+   printf("\nUsage: %s [-s <id>] <highest CPU load>\n"
+	  "\nExample: %s -s h 50\n\n", name, name);
+   printf("CPU load of 0 means random load, anything else is percentage (1-100).\n"
+	  "\nThe value given to '-s' can be used to set the scheduling priority/policy:\n"
+	  "\tl -- lowest nice() priority\n"
+	  "\th -- highest nice() priority\n"
+	  "\tb -- use SCHED_BATCH scheduler\n"
+	  "\tf -- use SCHED_FIFO scheduler (real time)\n"
+	  "\tr -- use SCHED_RR scheduler (real time)\n"
+	  "\to -- use SCHED_OTHER (default) scheduler\n"
+	  "\nSee \"man sched_setscheduler\" and \"man 2 nice\".\n");
+   return 1;
 }
