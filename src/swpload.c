70b1557224a4b2a7d8a44dfd3e2d7f40afbc598d
Tommi Rantala <ext-tommi.1.rantala@nokia.com>
2011-02-01 13:59:06 +0200

    swpload.c: GPLv2

--- src/swpload.c
+++ src/swpload.c
@@ -1,9 +1,26 @@
 /* ========================================================================= *
  * File: swpload.c
  *
- * Copyright (C) 2009 Nokia. All rights reserved.
+ * This file is part of sp-stress.
+ *
+ * Copyright (C) 2009 Nokia Corporation.
  *
  * Author: Leonid Moiseichuk <leonid.moiseichuk@nokia.com>
+ * Contact: Eero Tamminen <eero.tamminen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  *
  * Description:
  *    Stress paging system load test. This application occupis required
